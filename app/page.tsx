e43c6b4d07236c0a1170d1704bf30934eb2b1d90
dominicnaatey <dominicnaatey@gmail.com>
2025-12-22 11:06:37 +0000

    adjust padding and width in main layout

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import NeoCarousel from "@/components/neo-carousel/NeoCarousel";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-white">
-      <div className="z-10 max-w-5xl w-full items-center justify-center font-mono text-sm flex flex-col">
+    <main className="flex min-h-screen flex-col items-center justify-between py-24 bg-white">
+      <div className="z-10  w-full items-center justify-center font-mono text-sm flex flex-col">
         <h1 className="text-4xl font-bold mb-8 text-black">Neo Robotics Showcase</h1>
         <NeoCarousel />
       </div>
